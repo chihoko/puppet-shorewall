f47c85c0cabd3f9ec45628b29ffc2fb54fcb264b
Nick <nick@ivpn.net>
2015-08-15 00:18:00 +0200

    Replace location with country

--- manifests/tunnel.pp
+++ manifests/tunnel.pp
@@ -4,7 +4,7 @@ define shorewall::tunnel(
     $gateway = '0.0.0.0/0',
     $gateway_zones = '',
     $order = '1',
-    $location = undef,
+    $country = undef,
 ) {
     shorewall::entry { "tunnels-${order}-${name}":
         line => "# ${name}\n${tunnel_type} ${zone} ${gateway} ${gateway_zones}",
